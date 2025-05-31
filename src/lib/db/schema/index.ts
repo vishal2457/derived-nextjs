f5e3b1b7ea61b89267bcaee24bd72ddde1bbf6cc
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2025-05-31 17:35:51 +0530

    Remove unused product schema export from index.ts

--- src/lib/db/schema/index.ts
+++ src/lib/db/schema/index.ts
@@ -2,6 +2,5 @@ export * from "./user.schema";
 export * from "./account.schema";
 export * from "./session.schema";
 export * from "./verification.schema";
-export * from "./product.schema";
 
 // import new schema
